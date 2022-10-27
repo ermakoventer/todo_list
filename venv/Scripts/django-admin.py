d41679af6924d4714825567072ca5dbb8b46508e
ermakoventer <ermakoventer777@gmail.com>
2022-10-27 11:05:47 +0300

    Worked with posgresql

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!c:\users\user\desktop\todo_drf\venv\scripts\python.exe
+#!d:\newprojects\todo_list\venv\scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
